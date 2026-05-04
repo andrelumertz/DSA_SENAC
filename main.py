c5accccbc27b2e34155ecacb19271b6f23b9fb1c
André Lumertz <andrelumertzmartins@gmail.com>
2026-05-04 16:27:40 -0300

    feat: adicionar o quick sort

--- main.py
+++ main.py
@@ -74,7 +74,72 @@ class Lista:
             resultado.proximo = self._merge(esquerda, direita.proximo) 
         return resultado
     #End Merge Sort
+    
+    #Algotitmo de Quick Sort
+    def quick_sort(self,cabeca,fim):
+        #matodo principal do quick sort
+        #se a lista estiver vazia ou for um unico nó, para
+        if not cabeca or cabeca == fim:
+            return cabeca
+        
+        #particiona a lista e coloca o pivo no lugar certo
+        nova_cabeca, novo_fim, pivo = self_particionar(cabeca,fim)
+        
+        #se o pivo nao for o primeiro elemtno, ordena a parte esquerda da lista
+        if nova_cabeca != pivo:
+            aux = nova_cabeca 
+            while aux.proximo != pivo:
+                aux = aux.proximo
+            aux.proximo = None #isola a esquerda
             
+            nova_cabeca = self.quick_sort(nova_cabeca, aux)
+            
+            #reconecta o pivo a parte esquerda ordenada
+            aux = self._get_ultimo(nova_cabeca)
+            aux.proximo = pivo
+            
+        #ordena a parte da direita da lista apos o pivo
+        pivo.proximo  = self.quick_sort(pivo.proximo, novo_fim)
+        
+        return nova_cabeca
+    
+    def _particionar(self, cabeca, fim):
+        #Organiza os nós ao redor do pivo
+        pivo = fim
+        anterior = None
+        atual = cabeca
+        cauda = pivo
+        
+        nova_cabeca = None
+        
+        while atual != pivo:
+            if atual.valor < pivo.valor:
+                #o nó fica onde está
+                if nova_cabeca is None:
+                    nova_cabeca = atual
+                anterior = atual 
+                atual =  atual.proximo
+            else:
+                #O nó é maior: move ele para depois do pivo
+                if anterior:
+                    anterior.proximo = atual.proximo
+                proximo_temp = atual.proximo
+                atual.proximo =  None
+                cauda.proximo = atual
+                cauda = atual
+                atual = proximo_temp
+            
+            if nova_cabeca is None:
+                nova_cabeca = pivo
+                
+            return nova_cabeca, cauda, pivo
+        
+        def _get_ultimo(self, cabeca):
+            #auxiliar para achar o fim do trilho
+            while cabeca and cabeca.proximo:
+                cabeca = cabeca.proximo
+            return cabeca
+    
         
 #testes
     
@@ -92,3 +157,17 @@ if __name__ == "__main__":
     
     print("Apos o Merge Sort:")
     minha_lista.exibir()
+    
+    lista_quick = Lista()
+    for d in [30, 10, 45, 5, 20]:
+        lista_quick.adicionar(d)
+
+    print("Quick Sort antes")
+    lista_quick.exibir()
+
+    #Pega o ultimo nó para passar como parametro
+    ultimo_no = lista_quick._get_ultimo(lista_quick.cabeca)
+    lista_quick.cabeca = lista_quick.quick_sort(lista_quick.cabeca, ultimo_no)
+
+    print("Quick Sort depois")
+    lista_quick.exibir()
